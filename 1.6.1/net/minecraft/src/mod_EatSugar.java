56e909c5fbd1ba02bf101f6a25c308a7ab4190e1
modchu <kawd82jstskl@yahoo.co.jp>
2013-07-11 22:08:39 +0900

    up

---
+++ 1.6.1/net/minecraft/src/mod_EatSugar.java
@@ -0,0 +1,14 @@
+package net.minecraft.src;
+
+public class mod_EatSugar extends BaseMod
+{
+	public String getVersion()
+	{
+		return "1.6.1-1";
+	}
+
+	public void load()
+	{
+		Item.sugar = (new ItemFood(97, 2, 0.1F, false)).setUnlocalizedName("sugar").setPotionEffect(PotionHelper.sugarEffect).setCreativeTab(CreativeTabs.tabMaterials).func_111206_d("sugar");
+	}
+}
